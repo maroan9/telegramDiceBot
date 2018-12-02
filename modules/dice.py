6480a34dda9989ad8e31f9aafac7045f6911688f
Unknown <egaden87@gmail.com>
2018-12-02 12:44:09 +0100

    Mejorando la formato de resultados

--- modules/dice.py
+++ modules/dice.py
@@ -1,8 +1,41 @@
 import re
+from random import randrange
 
 # (\d+(?!d))|([\+\-\*\/])|(\d*d\d+)
-def leerDado(formula):
+
+
+def leerFormula(formula):
     if bool(re.match('^[1-9d]', formula)):
-        return formula
+        result = ''
+        f = ''
+        for g in re.finditer('(\d+(?!d))|([\+\-\*\/\(\)])|(\d*d\d+)', formula):
+            temp = g.group(0)
+            if 'd' in temp:
+                resDado = leerDado(temp)                
+                f += resDado
+                result += temp + ' (' + resDado + ') '
+            else:
+                f += temp
+                result += temp
+        try:
+            total = eval(f)            
+        except:
+            return 'El formato de la formula no es el correcta'       
+        
+        result += '=' + str(total)
+        return result
     else:
-        return 'El formato del dado no es el correcto'
+        return 'El formato de la formula no es el correcta'
+
+
+def leerDado(dado):
+    if dado[0] == 'd':
+        cantidad = 1
+    else:
+        cantidad = dado[0]
+    caras = dado[-1]
+    resultado = 0
+    for i in range(int(cantidad)):
+        resultado += randrange(int(caras)) + 1
+
+    return str(resultado)
