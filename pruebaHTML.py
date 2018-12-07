6ae76faa7e1ec9d959096734500e01a4575dc22d
Marcos Romeu <egaden87@gmail.com>
2018-12-07 13:36:32 +0100

    Imagenes dados de 8

--- pruebaHTML.py
+++ pruebaHTML.py
@@ -1,9 +1,11 @@
 import imgkit
 
+css = 'assets/dice.css'
 options = {
     'format': 'png',
     'crop-w': '120'
 }
+# imgkit.from_file('prueba.html', 'out2.png', options=options)
 imgkit.from_string(
-    '<div style="background-color:transparent">Hel <b>lo!</b><br><img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" /></div>',
-    'out2.png', options=options)
+    '<div class="d8_8"> </div>',
+    'out2.png', options=options, css=css)
