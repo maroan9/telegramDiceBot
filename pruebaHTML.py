98f9c87dd1a79c511b91b8d522efadbbfbad61f8
Unknown <egaden87@gmail.com>
2018-12-07 15:44:54 +0100

    pruebas html

--- pruebaHTML.py
+++ pruebaHTML.py
@@ -1,11 +1,22 @@
 import imgkit
 
+f = open('prueba.html', 'w')
+
 css = 'assets/dice.css'
 options = {
-    'format': 'png',
-    'crop-w': '120'
+    'format': 'svg',
+    'crop-w': '320'
 }
-# imgkit.from_file('prueba.html', 'out2.png', options=options)
-imgkit.from_string(
-    '<div class="d8_8"> </div>',
-    'out2.png', options=options, css=css)
+
+mensaje="""<html>
+<head></head>
+<body style="background-color: black"><p style="width:310; color:white;">Hola mundo!<img style="width:50px; height:50px; vertical-align: middle;" src="assets/d8_1.svg"> + <img style="width:50px; height:50px; vertical-align: middle;" src="assets/d8_5.svg"> + 5 + 6 + 7 + 8 + 9 = 100</p></body>
+</html>"""
+
+f.write(mensaje)
+f.close()
+
+imgkit.from_file('prueba.html', 'out2.svg', options=options)
+# imgkit.from_string(
+#     '<div class="d8_8"> </div>',
+#     'out2.png', options=options, css=css)
